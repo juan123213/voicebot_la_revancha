e05f179b536bec75c6d6b5b297d4a1a9b6e14e9c
Juan Andrés Giraldo <juan.andresgiraldo@hotmail.com>
2024-11-19 10:48:19 -0500

    modularizar asistente negociador

---
+++ negociador/config.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Base de datos de deudores
+DEBTORS_DB = pd.DataFrame({
+    'nombre': ['Miguel'],
+    'dni': ['1234'],
+    'deuda': [5000.00],
+    'estado': ['activo']
+})
+
+# Claves de API
+API_KEYS = [
+    "AIzaSyBG0LMFQgXmkxlIbx-kvmrFzBgZFMbyR-g",
+    "AIzaSyBhnvqxLjhzfbUp3MnFjwEMsNJ4VYY7r3A",
+    "AIzaSyB5wvYrrT1DzA4bH4oRLuO0lF4TS3fMiw8",
+    "AIzaSyDKj4QW99q9Kfvues0AtSmGRGNqhYrUr7A",
+    "AIzaSyCIt5-vZ45sYP-VlEF98fFbpZAbWOmdNz0",
+    "AIzaSyDKzJgxKgHo8mOZ7pJueCRW57x0OQYObBY",
+    "AIzaSyD9Bu2jX6jXbuuaTW2Sjh4hhUeuJdYD23s"
+]
